9b8328760ff7d35fd8700b946a790a1c49884b6f
Ibrahim_zakaria <i.zakaria@st.ul.edu.lb>
2021-04-06 14:17:56 +0300

    add Todo Service

---
+++ src/app/shared/todo.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  todos:Todo[]=[]
+  constructor() { }
+
+  getTodos(){
+    return this.todos
+  }
+
+  getTodo(id:string){
+    return this.todos.find(t=>t.id === id)
+  }
+
+  addTodo(todo:Todo){
+    this.todos.push(todo)
+  }
+
+  updateTodo(id:string,updateTodoFields:Partial<Todo>){
+    const todo = this.getTodo(id)
+    Object.assign(todo,updateTodoFields)
+  }
+
+  DeleteTodo(id:string){
+    const index = this.todos.findIndex(t=>t.id===id)
+    if(index == -1) return
+
+    this.todos.splice(index,-1)
+
+  }
+}
