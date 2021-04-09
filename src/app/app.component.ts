f1c92e6f7e20387e63095d43823453728bf72f14
Ibrahim_zakaria <i.zakaria@st.ul.edu.lb>
2021-04-09 11:32:46 +0300

    Route Transitions for secondary routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import {
-  animate,
-  group,
-  query,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
+import {animate,group,query,style,transition,trigger,}
+from '@angular/animations';
 import { Component } from '@angular/core';
-import {  RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+
+const BaseStyle = style({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  display: 'block',
+});
 
 @Component({
   selector: 'app-root',
@@ -21,20 +24,7 @@ import {  RouterOutlet } from '@angular/router';
           overflow: 'hidden',
         }),
 
-        query(
-          ':enter,:leave',
-          [
-            style({
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              display: 'block',
-            }),
-          ],
-          { optional: true }
-        ),
+        query(':enter,:leave', [BaseStyle], { optional: true }),
 
         //  query(':enter',[
         //    style({ opacity:0 })
@@ -81,20 +71,7 @@ import {  RouterOutlet } from '@angular/router';
           overflow: 'hidden',
         }),
 
-        query(
-          ':enter,:leave',
-          [
-            style({
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              display: 'block',
-            }),
-          ],
-          { optional: true }
-        ),
+        query(':enter,:leave', [BaseStyle], { optional: true }),
 
         //  query(':enter',[
         //    style({ opacity:0 })
@@ -134,62 +111,151 @@ import {  RouterOutlet } from '@angular/router';
           ),
         ]),
       ]),
-    ]),
-    trigger('bgAnim',[
-      transition(':leave',[
-        animate(1000,style({
-          opacity:0
-        }))
-      ])
-    ]),
-    trigger('fadeAnim',[
-      transition(':enter',[
+
+      transition('*=> secondary', [
         style({
-          opacity:0
+          position: 'relative',
         }),
-        animate(250,style({
-          opacity:1
-        }))
+
+        query(':enter,:leave', [BaseStyle], { optional: true }),
+
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({
+                  opacity: 0,
+                  transform: 'scale(0.8)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+
+          query(
+            ':enter',
+            [
+              style({
+                transform: 'scale(1.2)',
+                opacity: 0,
+              }),
+              animate(
+                '250ms 120ms ease-out',
+                style({
+                  opacity: 1,
+                  transform: 'scale(1)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
       ]),
-      transition(':leave',[
-        animate(250,style({
-          opacity:0
-        }))
-      ])
-    ])
-  ]
+      transition('secondary => *', [
+        style({
+          position: 'relative',
+        }),
+
+        query(':enter,:leave', [BaseStyle], { optional: true }),
+
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({
+                  opacity: 0,
+                  transform: 'scale(1.25)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+
+          query(
+            ':enter',
+            [
+              style({
+                transform: 'scale(0.8)',
+                opacity: 0,
+              }),
+              animate(
+                '250ms 120ms ease-out',
+                style({
+                  opacity: 1,
+                  transform: 'scale(1)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+    ]),
+    trigger('bgAnim', [
+      transition(':leave', [
+        animate(
+          1000,
+          style({
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+    trigger('fadeAnim', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+        }),
+        animate(
+          250,
+          style({
+            opacity: 1,
+          })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          250,
+          style({
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  bgs?: string []=[
-    'https://images.unsplash.com/photo-1616626578044-e5689b3aa390?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixlib=rb-1.2.1&q=80&w=1920'
-    
-  ]
-
-
-    loadingbg?:boolean
-
+  bgs?: string[] = [
+    'https://images.unsplash.com/photo-1616626578044-e5689b3aa390?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixlib=rb-1.2.1&q=80&w=1920',
+  ];
+  loadingbg?: boolean;
   async changebg() {
-
-    this.loadingbg=true
+    this.loadingbg = true;
     const result = await fetch(
       'https://source.unsplash.com/1920x1080/?nature,mountains',
       {
         method: 'HEAD',
       }
     );
- 
+
     this.bgs?.push(result.url);
   }
-  onbgload(imgEvent:Event){
-
+  onbgload(imgEvent: Event) {
     // remove the old bg image
-    const imgelet = imgEvent.target as HTMLImageElement
-    const src = imgelet.src
-    this.bgs = this.bgs?.filter(b => b ===src)
-    this.loadingbg=false
+    const imgelet = imgEvent.target as HTMLImageElement;
+    const src = imgelet.src;
+    this.bgs = this.bgs?.filter((b) => b === src);
+    this.loadingbg = false;
   }
   prepareRoute(outlet: RouterOutlet) {
-    if (outlet.isActivated) return outlet.activatedRouteData['tab'];
-    else return;
+    if (outlet.isActivated) {
+      const tab = outlet.activatedRouteData['tab'];
+      if (!tab) return 'secondary';
+      return tab;
+    }
   }
 }
